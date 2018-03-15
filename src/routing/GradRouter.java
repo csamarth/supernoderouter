66dd2e08baa816debc3061b1efb7dea2f6cffd97
Shubham Kumaram <shubhamkumaram@gmail.com>
2018-03-15 12:44:28 +0530

    Implemented getTheta, getAlpha, getSlope and getRadioRange

--- src/routing/GradRouter.java
+++ src/routing/GradRouter.java
@@ -2,26 +2,23 @@ package routing;
 
 import core.Coord;
 import core.DTNHost;
+import core.NetworkInterface;
 import core.Settings;
 
 public class GradRouter extends ActiveRouter {
-
+	
 	public GradRouter(Settings s) {
 		super(s);
-		// TODO Auto-generated constructor stub
 	}
 
 	public GradRouter(ActiveRouter r) {
 		super(r);
-		// TODO Auto-generated constructor stub
 	}
-	
-	
 	
 	private double getProbability(DTNHost from, DTNHost to){
 		Coord fromCoord = from.getLocation();
 		Coord toCoord = to.getLocation();
-		int toRadioRange = getRadioRange(to);
+		double toRadioRange = getRadioRange(to);
 		double alpha = getAlpha(fromCoord, toCoord, to.getPath().getNextWaypoint());
 		double theta = getTheta(fromCoord, toCoord, toRadioRange);
 		
@@ -30,25 +27,31 @@ public class GradRouter extends ActiveRouter {
 		return probability;
 	}
 
-	private double getTheta(Coord fromCoord, Coord toCoord, int toRadioRange) {
-		// TODO Auto-generated method stub
-		return 0;
+	private double getTheta(Coord fromCoord, Coord toCoord, double toRadioRange) {
+		double distance = fromCoord.distance(toCoord) ;
+		double theta = Math.atan(toRadioRange/distance);
+		return theta;
 	}
 
 	private double getAlpha(Coord fromCoord, Coord toCoord, Coord nextWaypoint) {
-		// TODO Auto-generated method stub
-		return 0;
+		double slopePath = getSlope(fromCoord, nextWaypoint);
+		double slopeDest = getSlope(fromCoord, toCoord);
+		double alpha = Math.abs(Math.atan(slopePath) - Math.atan(slopeDest));
+		return alpha;
 	}
 
-	private int getRadioRange(DTNHost to) {
-		// TODO Auto-generated method stub
-		return 0;
+	private double getSlope(Coord from, Coord to) {
+		double slope = (to.getY() - from.getY())/(to.getX() - from.getX());
+		return slope;
+	}
+
+	private double getRadioRange(DTNHost to) {
+		return (double)getHost().getComBus().getProperty(NetworkInterface.RANGE_ID);
 	}
 
 	@Override
 	public MessageRouter replicate() {
-		// TODO Auto-generated method stub
-		return null;
+		GradRouter r = new GradRouter(this);	
+		return r;
 	}
-
 }
